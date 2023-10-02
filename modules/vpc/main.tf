ba41e74300e746b8828c3f01e57d03b1ae70c03e
India <India@India-PC>
2023-10-02 14:14:52 +0530

    mod1

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -40,4 +40,13 @@
 	    Name = "${var.env}-ngw"
 	  }
 	}
+	
+	resource "aws_nat_gateway" "ngw" {
+	  allocation_id = aws_eip.ngw.id
+	  subnet_id     = aws_subnet.public_subnets[0].id
+	
+	  tags = {
+	    Name = "${var.env}-ngw"
+	  }
+	}
 		
