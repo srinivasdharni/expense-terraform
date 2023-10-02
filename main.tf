7043c46f1d191892b216663c082085e6cc10e7b8
India <India@India-PC>
2023-10-02 14:55:35 +0530

    modi

--- main.tf
+++ main.tf
@@ -5,4 +5,8 @@
 	  private_subnets = var.private_subnets
 	  public_subnets  = var.public_subnets
 	  azs             = var.azs
+	  account_no             = var.account_no
+ 	  default_vpc_id         = var.default_vpc_id
+ 	  default_vpc_cidr       = var.default_vpc_cidr
+  	  default_route_table_id = var.default_route_table_id
 	}
