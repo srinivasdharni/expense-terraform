1d1caa221a5ce8cf6f99fc631d29a61bc050b43d
India <India@India-PC>
2023-10-02 14:23:27 +0530

    mod2

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -49,4 +49,36 @@
 	    Name = "${var.env}-ngw"
 	  }
 	}
+	
+	resource "aws_route_table" "public" {
+	  vpc_id = aws_vpc.main.id
+	
+	  route {
+	    cidr_block = "0.0.0.0/0"
+	    gateway_id = aws_internet_gateway.igw.id
+	  }
+	
+	  tags = {
+	    Name = "public"
+	  }
+	}
+	
+	resource "aws_route_table" "private" {
+	  vpc_id = aws_vpc.main.id
+	
+	  route {
+	    cidr_block     = "0.0.0.0/0"
+	    nat_gateway_id = aws_nat_gateway.ngw.id
+	  }
+	
+	  route {
+	    cidr_block                = var.default_vpc_cidr
+	    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+	  }
+	
+	  tags = {
+	    Name = "private"
+	  }
+	}
 		
+			
