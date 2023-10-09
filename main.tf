7080678e949d1d27bf44f479e4385084dac26324
India <India@India-PC>
2023-10-09 14:28:57 +0530

    modified

--- main.tf
+++ main.tf
@@ -11,47 +11,7 @@
 	  	  default_route_table_id = var.default_route_table_id
 		}
 		
-	module "public-lb" {
-	  source            = "./modules/alb"
-	  alb_sg_allow_cidr = "0.0.0.0/0"
-	  alb_type          = "public"
-	  env               = var.env
-	  internal          = false
-	  subnets           = module.vpc.public_subnets
-	  vpc_id            = module.vpc.vpc_id
-	}
 	
-	module "private-lb" {
-	  source            = "./modules/alb"
-	  alb_sg_allow_cidr = var.vpc_cidr
-	  alb_type          = "private"
-	  env               = var.env
-	  internal          = true
-	  subnets           = module.vpc.private_subnets
-	  vpc_id            = module.vpc.vpc_id
-	}
-	
-	module "frontend" {
-	  source = "./modules/app"
-	  app_port      = 80
-	  component     = "frontend"
-	  env           = var.env
-	  instance_type = "t3.micro"
-	  vpc_cidr      = var.vpc_cidr
-	  vpc_id        = module.vpc.vpc_id
-	  subnets       = module.vpc.private_subnets
-	}
-	
-	module "backend" {
-	  source = "./modules/app"
-	  app_port      = 8080
-	  component     = "backend"
-	  env           = var.env
-	  instance_type = "t3.micro"
-	  vpc_cidr      = var.vpc_cidr
-	  vpc_id        = module.vpc.vpc_id
-	  subnets       = module.vpc.private_subnets
-	}
 	
 	module "mysql" {
 	  source = "./modules/rds"
