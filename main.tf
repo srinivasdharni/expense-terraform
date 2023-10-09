8508ec584b0809ca81f3626856943d9299e89952
India <India@India-PC>
2023-10-09 15:38:41 +0530

    m

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@
 	  vpc_cidr      = var.vpc_cidr
 	  vpc_id        = module.vpc.vpc_id
 	  subnets       = module.vpc.private_subnets
+	  bastion_node_cidr = var.bastion_node_cidr
 	}
 		
 	module  "mysql" {
