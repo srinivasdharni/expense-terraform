614efeac056722ba302ca6d54bd3f61d3633a993
India <India@India-PC>
2023-10-09 17:46:24 +0530

    m

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -121,4 +121,13 @@
 	  port     = var.app_port
 	  protocol = "HTTP"
 	  vpc_id   = var.vpc_id
+	  health_check {
+	    enabled             = true
+	    healthy_threshold   = 2
+	    interval            = 5
+	    unhealthy_threshold = 2
+	    port                = var.app_port
+	    path                = "/health"
+	    timeout             = 3
+  		}
 	}
