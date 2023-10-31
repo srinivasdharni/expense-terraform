42c0d872f65105682bb129991f8180732a6a8ee6
India <India@India-PC>
2023-10-31 23:29:52 +0530

    m

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -6,3 +6,30 @@ resource "aws_eks_cluster" "main" {
     subnet_ids = var.subnet_ids
   }
 }
+
+resource "aws_iam_role" "main" {
+  name = "${var.env}-${var.project_name}-eks-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.main.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.main.name
+}
